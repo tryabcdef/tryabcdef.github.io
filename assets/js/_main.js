3e980ea3cd212893fc14179639dbc7c6256c78fb
tryabcdef <117263673+tryabcdef@users.noreply.github.com>
2025-01-22 16:47:45 +0530

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,19 +1,13 @@
 /* ==========================================================================
    jQuery plugin settings and other scripts
    ========================================================================== */
-
 $(document).ready(function(){
-  // These should be the same as the settings in _variables.scss
-  scssLarge = 925; // pixels
-
-  // Sticky footer
+  // Existing code - Sticky footer
   var bumpIt = function() {
       $("body").css("margin-bottom", $(".page__footer").outerHeight(true));
     },
     didResize = false;
-
   bumpIt();
-
   $(window).resize(function() {
     didResize = true;
   });
@@ -40,10 +34,10 @@ $(document).ready(function(){
     }
   });    
 
-  // init smooth scroll, this needs to be slightly more than then fixed masthead height
+  // Smooth scroll
   $("a").smoothScroll({offset: -65});
 
-  // add lightbox class to all image links
+  // Add lightbox class to all image links
   $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
 
   // Magnific-Popup options
@@ -53,23 +47,30 @@ $(document).ready(function(){
     gallery: {
       enabled: true,
       navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      preload: [0,1]
     },
     image: {
       tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
     },
-    removalDelay: 500, // Delay in milliseconds before popup is removed
-    // Class that is added to body when popup is open.
-    // make it unique to apply your CSS animations just to this exact popup
+    removalDelay: 500,
     mainClass: 'mfp-zoom-in',
     callbacks: {
       beforeOpen: function() {
-        // just a hack that adds mfp-anim class to markup
         this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
       }
     },
     closeOnContentClick: true,
-    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    midClick: true
   });
 
+  // New code for typing animation control
+  setTimeout(() => {
+    const heading = document.querySelector('.hero-section h1');
+    if (heading) {
+      heading.classList.add('typed');
+    }
+  }, 1500);  // Stops cursor blinking after 1.5 seconds
+
+  // Set initially visible elements
+  $('.hero-section h1').css('opacity', '1');
 });
